c4e38b9590a2e2d94962ae5ed141f2c9d24e0225
blz-soft <blz.soft@outlook.jp>
2021-07-11 17:53:16 +0900

    Added the ability to output colors.
    Signed-off-by: blz-soft <blz.soft@outlook.jp>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+// Copyright (c) 2021 Daichi Aoki  
+// Released under the [MIT license](https://github.com/blz-soft/happy_server/blob/main/LICENSE)  
+
 use actix_files;
 use actix_web::{App, HttpServer, dev::Server};
+// Add color to console output
+use colored::*;
 
 // Compile-time defaults
 #[cfg(any(feature = "japanese",not(feature = "english")))]
 const COMPILE_TIME_DEFAULT_LANGUAGE:Language = Language::Japanese;
 #[cfg(feature = "english")]
 const COMPILE_TIME_DEFAULT_LANGUAGE:Language = Language::English;
+
+#[cfg(not(feature = "no_color"))]
+const COMPILE_TIME_DEFAULT_COLOR: bool = true;
+#[cfg(feature = "no_color")]
+const COMPILE_TIME_DEFAULT_COLOR: bool = false;
+
+
 
 /// Language settings
 #[allow(dead_code)]
@@ -14,12 +26,46 @@ enum Language {
     English
 }
 
+#[allow(dead_code)]
+struct StyledString {
+    error: String,
+    note: String,
+    running: String,
+    finish: String
+} impl StyledString {
+    fn colored() -> Self {
+        Self{
+            error: "Error".red().bold().to_string(),
+            note: "Note".blue().bold().to_string(),
+            running: "Running".green().bold().to_string(),
+            finish: "Finish".green().bold().to_string(),
+        }
+    }
+    fn no_colored() -> Self {
+        Self {
+            error: "Error".to_string(),
+            note: "Note".to_string(),
+            running: "Running".to_string(),
+            finish: "Finish".to_string(),
+        }
+    }
+}
+
 /// # application entry point
 /// Note: Although it is an async function, it is converted to a normal function signature by the #[actix_web::main] attribute.
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // language setting
     let language = COMPILE_TIME_DEFAULT_LANGUAGE;
+    // cli color setting  ex) true = colored , false = no_colored
+    let cli_color = COMPILE_TIME_DEFAULT_COLOR;
+
+    // cli style strings
+    let styled_string = if cli_color {
+        StyledString::colored()
+    } else {
+        StyledString::no_colored()
+    };
 
     // start web server
     let web_server = start_server().await;
@@ -28,8 +74,8 @@ async fn main() -> std::io::Result<()> {
             // Output when the web server fails to start.
             // case of cli
             let output_message = match language{
-                Language::Japanese => format!("Error: カレントディレクトリをhttpで配信できませんでした。"),
-                Language::English => format!("Error: The current directory could not be delivered via http.")
+                Language::Japanese => format!("{error}: カレントディレクトリをhttpで配信できませんでした。", error=styled_string.error),
+                Language::English => format!("{error}: The current directory could not be delivered via http.", error=styled_string.error)
             };
             println!("{}", output_message);
             std::process::exit(0);
@@ -38,12 +84,12 @@ async fn main() -> std::io::Result<()> {
             // Output when the web server is successfully started.
             // case of cli
             let output_message = match language{
-                Language::Japanese => format!("Running: カレントディレクトリをhttpで配信しています。\n\
+                Language::Japanese => format!("{running}: カレントディレクトリをhttpで配信しています。\n\
                 http://localhost にアクセスすればブラウズができます。\n\n\
-                終了する場合は、Ctrl + C を押すか、このウィンドを閉じてください。"),
-                Language::English => format!("Running: The current directory is served by http!!\n\
+                終了する場合は、Ctrl + C を押すか、このウィンドを閉じてください。", running=styled_string.running),
+                Language::English => format!("{running}: The current directory is served by http!!\n\
                 You can browse by visiting http://localhost. \n\n\
-                To exit, press Ctrl + C or close this window.")
+                To exit, press Ctrl + C or close this window.", running=styled_string.running)
             };
             println!("{}", output_message);
 
