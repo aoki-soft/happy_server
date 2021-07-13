6ab092600a0869d6ba0e0092f6a7e7f2d4734397
blz-soft <blz.soft@outlook.jp>
2021-07-13 23:39:06 +0900

    Added a test.

--- src/main.rs
+++ src/main.rs
@@ -140,8 +140,6 @@ async fn main() -> io::Result<()> {
     let ipv4_addr = DEFAULT_IPV4_ADDR;
     let port = DEFAULT_HTTP_PORT;
 
-
-
     // viewer setup
     let viewer = Viewer::Cli(CliViewer{language, style});
     // TODO: WebApi
@@ -165,4 +163,53 @@ async fn main() -> io::Result<()> {
 
     // Wait for it to finish.
     web_server.await
+}
+
+#[cfg(test)]
+mod test {
+    use actix_web::client::Client;
+    use std::thread;
+    use std::time::Duration;
+    use std::sync::Arc;
+    use std::fs::File;
+    use std::io::Write;
+
+    #[test]
+    fn get_file() {
+
+        let test_file_text = Arc::new("Hello World!! \n 日本語です。".to_owned());
+        // let mut test_file_path = std::env::current_exe().unwrap().parent().unwrap().to_owned();
+        let mut test_file_path = std::env::current_dir().unwrap();
+        let test_file_name = "test_file.txt";
+        test_file_path.push(test_file_name);
+        let mut test_file = File::create(test_file_path.clone()).unwrap();
+        test_file.write(test_file_text.as_bytes()).unwrap();
+
+
+        let _main_handle = thread::spawn(|| {
+            let _ = super::main();
+        });
+
+        let test_handle = thread::spawn(|| {test_request(test_file_text)});
+        test_handle.join().unwrap();
+
+        std::fs::remove_file(test_file_path).unwrap();
+    }
+
+    #[actix_web::main]
+    async fn test_request(test_file_text: Arc<String>) {
+        thread::sleep(Duration::from_millis(1));
+
+        // request
+        let client = Client::default();
+        let mut res = client.get("http://localhost/test_file.txt") // <- Create request builder
+            .header("User-Agent", "Actix-web")
+            .send()                             // <- Send http request
+            .await.unwrap();
+        
+        let res = res.body();
+        let res = res.await.unwrap();
+
+        assert_eq!(res, *test_file_text.as_bytes());
+    }
 }
